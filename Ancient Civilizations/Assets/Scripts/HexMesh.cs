7208ff28f76111014b322736203aa8d4299d4a29
Yazip <USAASU386@mail.ru>
2024-06-23 17:47:26 +0700

    Made ledges on the edges of the cells

--- Ancient Civilizations/Assets/Scripts/HexMesh.cs
+++ Ancient Civilizations/Assets/Scripts/HexMesh.cs
@@ -85,10 +85,11 @@ public class HexMesh : MonoBehaviour
         Vector3 v4 = v2 + bridge;
         v3.y = v4.y = neighbor.Elevation * HexMetrics.elevationStep;
 
-        // Создаём четырёхугольник-соединение
+        // Создаём уступы на ребре
 
-        AddQuad(v1, v2, v3, v4);
-        AddQuadColor(cell.color, neighbor.color);
+        TriangulateEdgeTerraces(v1, v2, cell, v3, v4, neighbor);
+        //AddQuad(v1, v2, v3, v4);
+        //AddQuadColor(cell.color, neighbor.color);
 
         // Заполняем угловые пробелы
 
@@ -100,6 +101,38 @@ public class HexMesh : MonoBehaviour
             AddTriangle(v2, v4, v5);
             AddTriangleColor(cell.color, neighbor.color, nextNeighbor.color);
         }
+    }
+
+    // Метод для триангуляции уступов на ребре
+
+    void TriangulateEdgeTerraces(
+        Vector3 beginLeft, Vector3 beginRight, HexCell beginCell,
+        Vector3 endLeft, Vector3 endRight, HexCell endCell
+    )
+    {
+        Vector3 v3 = HexMetrics.TerraceLerp(beginLeft, endLeft, 1);
+        Vector3 v4 = HexMetrics.TerraceLerp(beginRight, endRight, 1);
+        Color c2 = HexMetrics.TerraceLerp(beginCell.color, endCell.color, 1);
+
+        // Создаём уступы
+        
+        AddQuad(beginLeft, beginRight, v3, v4);
+        AddQuadColor(beginCell.color, c2);
+
+        for (int i = 2; i < HexMetrics.terraceSteps; i++)
+        {
+            Vector3 v1 = v3;
+            Vector3 v2 = v4;
+            Color c1 = c2;
+            v3 = HexMetrics.TerraceLerp(beginLeft, endLeft, i);
+            v4 = HexMetrics.TerraceLerp(beginRight, endRight, i);
+            c2 = HexMetrics.TerraceLerp(beginCell.color, endCell.color, i);
+            AddQuad(v1, v2, v3, v4);
+            AddQuadColor(c1, c2);
+        }
+
+        AddQuad(v3, v4, endLeft, endRight);
+        AddQuadColor(c2, endCell.color);
     }
 
     // Метод для добавления цветов треугольника
